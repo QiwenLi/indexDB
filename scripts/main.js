6d33ec042ebe6b5cb1e13d0d15d2f56877fda429
qiwen.li <qiwen.li@qunar.com>
2017-02-28 17:38:47 +0800

    firstPush

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,26 @@
 /**
  * Created by liqiwen on 17/2/28.
  */
+
+;
+$(function () {
+    var myDB = {
+        name: 'test',
+        version: '0.0.1',
+        data: null
+    };
+    function creatDatabase (dname, dversion) {
+        var request = window.indexDB.open(dname, dversion);
+        request.onerror = function (e) {
+            console.log('database error: ', e);
+        };
+        request.onsuccess = function () {
+            console.log('database success!');
+            myDB.data = request.result
+        };
+        request.onupgradeneeded = function () {
+            console.log('database version changed to: ' + dversion);
+        };
+    }
+    creatDatabase(myDB.name, myDB.version);
+});
