f557f133f395df01103bdf1dbe66948c7e31c664
qiwen.li <qiwen.li@qunar.com>
2017-02-28 17:47:01 +0800

    firstPush

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@
 $(function () {
     var myDB = {
         name: 'test',
-        version: '0.0.1',
+        version: '1.0',
         data: null
     };
     function creatDatabase (dname, dversion) {
